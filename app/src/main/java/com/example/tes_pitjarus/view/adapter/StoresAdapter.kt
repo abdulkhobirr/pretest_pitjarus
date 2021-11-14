49bf8a52290537fe8b679cad1fa1c7ff0d8616b5
abdulkhobir <abdulkhobir27@gmail.com>
2021-11-14 20:55:48 +0700

    Added stores adapter

---
+++ app/src/main/java/com/example/tes_pitjarus/view/adapter/StoresAdapter.kt
@@ -0,0 +1,60 @@
+package com.example.tes_pitjarus.view.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.NonNull
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tes_pitjarus.data.stores.local.entity.Stores
+import com.example.tes_pitjarus.databinding.ItemStoreBinding
+
+class StoresAdapter (
+    val data: MutableList<Stores> = mutableListOf(),
+    val listener: OnStoreClicked
+    ): RecyclerView.Adapter<StoresAdapter.ViewHolder>() {
+    fun setStoresData(list: List<Stores>) {
+        if (data.isNotEmpty()) {
+            data.clear()
+        }
+        data.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    fun clearData(){
+        data.clear()
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoresAdapter.ViewHolder {
+        val binding = ItemStoreBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return StoreViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: StoresAdapter.ViewHolder, position: Int) {
+        val item: Stores = data[position]
+        val storeViewHolder = holder as StoreViewHolder
+        storeViewHolder.bindStore(item)
+    }
+
+    open inner class ViewHolder(@NonNull itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    inner class StoreViewHolder(private val binding: ItemStoreBinding): ViewHolder(binding.root) {
+        fun bindStore(data: Stores){
+            binding.apply {
+                tvNamaToko.text = data.store_name
+                tvCluster.text = data.dc_name
+                mcvStore.setOnClickListener {
+                    listener.onStoreClicked(data)
+                }
+            }
+        }
+    }
+
+    interface OnStoreClicked{
+        fun onStoreClicked(data: Stores)
+    }
+}
--- app/src/main/java/com/example/tes_pitjarus/view/fragments/VisitationFragment.kt
+++ app/src/main/java/com/example/tes_pitjarus/view/fragments/VisitationFragment.kt
@@ -5,11 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.tes_pitjarus.data.stores.local.entity.Stores
 import com.example.tes_pitjarus.databinding.FragmentVisitationBinding
+import com.example.tes_pitjarus.utils.*
+import com.example.tes_pitjarus.utils.viewmodel.ResultWrapper
+import com.example.tes_pitjarus.view.adapter.StoresAdapter
+import com.example.tes_pitjarus.viewmodels.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class VisitationFragment : Fragment() {
+class VisitationFragment : Fragment(), StoresAdapter.OnStoreClicked {
     private var _binding: FragmentVisitationBinding?=null
     private val binding get() = _binding!!
+
+    private val mainViewModel: MainViewModel by viewModel()
+    private lateinit var storeAdapter: StoresAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +33,55 @@ class VisitationFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentVisitationBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initRV()
+        initObservable()
+        mainViewModel.getStores()
+    }
+
+    private fun initRV(){
+        storeAdapter = StoresAdapter(listener = this)
+
+        binding.rvListKunjungan.apply {
+            layoutManager =
+                LinearLayoutManager(
+                    context,
+                    LinearLayoutManager.VERTICAL,
+                    false
+                )
+            setHasFixedSize(true)
+            adapter = storeAdapter
+        }
+    }
+
+    override fun onStoreClicked(data: Stores) {
+        Toast.makeText(requireContext(), data.store_name, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun initObservable(){
+        mainViewModel.storeData.observe(viewLifecycleOwner, {
+            when(it){
+                is ResultWrapper.Empty -> {
+                    binding.msvListKunjungan.showEmptyState()
+                }
+                is ResultWrapper.Failure -> {
+                    binding.msvListKunjungan.showErrorState(it.message, it.title){
+                        mainViewModel.getStores()
+                    }
+                }
+                is ResultWrapper.Loading -> {
+                    binding.msvListKunjungan.showLoadingState()
+                }
+                is ResultWrapper.Success -> {
+                    binding.msvListKunjungan.showDefaultState()
+                    storeAdapter.setStoresData(it.data)
+                }
+            }
+        })
     }
 
     override fun onDestroyView() {
